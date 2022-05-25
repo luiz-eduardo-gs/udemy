fc04f40f8f58246ff5ea9331cae753ef62625e83
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-05-25 07:10:05 -0300

    feat: created game agent

--- elixir-phoenix-do-zero/ex_mon/lib/ex_mon.ex
+++ elixir-phoenix-do-zero/ex_mon/lib/ex_mon.ex
@@ -1,7 +1,13 @@
 defmodule ExMon do
   alias ExMon.Player
+  alias ExMon.Game
 
   def create_player(name, mov_avg, mov_rnd, mov_heal) do
     Player.build(name, mov_avg, mov_rnd, mov_heal)
+  end
+
+  def start_game(player) do
+    computer = Player.build("Robotik", :punch, :kick, :heal)
+    Game.start(computer, player)
   end
 end
