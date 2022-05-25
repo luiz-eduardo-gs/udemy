1328f185f12e75ba76fc1a7408ffbf182cfd5d2e
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-05-25 07:20:43 -0300

    feat: print game status

--- elixir-phoenix-do-zero/ex_mon/lib/ex_mon.ex
+++ elixir-phoenix-do-zero/ex_mon/lib/ex_mon.ex
@@ -1,12 +1,17 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
+
+  @computer_name "Robotinik"
 
   def create_player(name, mov_avg, mov_rnd, mov_heal) do
     Player.build(name, mov_avg, mov_rnd, mov_heal)
   end
 
   def start_game(player) do
-    Player.build("Robotik", :punch, :kick, :heal)
+    Player.build(@computer_name, :punch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
