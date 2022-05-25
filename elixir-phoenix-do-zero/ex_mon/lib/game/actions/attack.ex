12c0b210b950c12d1cd669c15f9d4c7c6e70a83d
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-05-25 08:04:21 -0300

    feat: print move massage

--- elixir-phoenix-do-zero/ex_mon/lib/game/actions/attack.ex
+++ elixir-phoenix-do-zero/ex_mon/lib/game/actions/attack.ex
@@ -1,5 +1,6 @@
 defmodule ExMon.Game.Actions.Attack do
   alias ExMon.Game
+  alias ExMon.Game.Status
 
   @mov_avg_power 18..25
   @mov_rnd_power 10..25
@@ -11,7 +12,7 @@ defmodule ExMon.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:life)
     |> calculate_total_life(damage)
-    |> update_opponent_life(opponent)
+    |> update_opponent_life(opponent, damage)
   end
 
   defp calculate_power(:mov_avg), do: Enum.random(@mov_avg_power)
@@ -20,16 +21,18 @@ defmodule ExMon.Game.Actions.Attack do
   defp calculate_total_life(life, damage) when life - damage < 0, do: 0
   defp calculate_total_life(life, damage), do: life - damage
 
-  defp update_opponent_life(life, opponent) do
+  defp update_opponent_life(life, opponent, damage) do
     opponent
     |> Game.fetch_player()
     |> Map.put(:life, life)
-    |> update_game(opponent)
+    |> update_game(opponent, damage)
   end
 
-  defp update_game(player_struct, opponent) do
+  defp update_game(player_struct, opponent, damage) do
     Game.info()
     |> Map.put(opponent, player_struct)
     |> Game.update()
+
+    Status.print_move_message(opponent, :attack, damage)
   end
 end
--- elixir-phoenix-do-zero/ex_mon/lib/game/status.ex
+++ elixir-phoenix-do-zero/ex_mon/lib/game/status.ex
@@ -11,4 +11,10 @@ defmodule ExMon.Game.Status do
     IO.puts("\n===== Movement #{move} is invalid =====\n")
     IO.puts("------------------------------")
   end
+
+  def print_move_message(:computer, :attack, damage),
+    do: IO.puts("\n===== The Player attacked the computer dealing #{damage} damage. =====\n")
+
+  def print_move_message(:player, :attack, damage),
+    do: IO.puts("\n===== The Computer attacked the player dealing #{damage} damage. =====\n")
 end
