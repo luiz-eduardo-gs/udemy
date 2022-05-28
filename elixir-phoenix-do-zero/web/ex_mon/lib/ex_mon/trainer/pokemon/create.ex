c5ecd825dc22401534ebe750fc883efe8ac4df82
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-05-28 08:27:54 -0300

    feat: validate if trainer exists

--- elixir-phoenix-do-zero/web/ex_mon/lib/ex_mon/trainer/pokemon/create.ex
+++ elixir-phoenix-do-zero/web/ex_mon/lib/ex_mon/trainer/pokemon/create.ex
@@ -2,6 +2,7 @@ defmodule ExMon.Trainer.Pokemon.Create do
   alias ExMon.PokeApi.Client
   alias ExMon.Pokemon, as: Parser
   alias ExMon.Trainer.Pokemon
+  alias ExMon.Trainer
   alias ExMon.Repo
 
   def call(%{"name" => name} = params) do
@@ -31,9 +32,21 @@ defmodule ExMon.Trainer.Pokemon.Create do
     }
 
     params
+    |> trainer_exists?()
+  end
+
+  defp trainer_exists?(%{trainer_id: trainer_id} = params) do
+    Repo.get(Trainer, trainer_id)
+    |> handle_get_trainer(params)
+  end
+
+  defp handle_get_trainer(%Trainer{} = _trainer, params) do
+    params
     |> Pokemon.build()
     |> handle_build()
   end
+
+  defp handle_get_trainer(nil, _params), do: {:error, "Trainer doesn't exist"}
 
   defp handle_build({:ok, pokemon}), do: Repo.insert(pokemon)
   defp handle_build({:error, _changeset} = error), do: error
