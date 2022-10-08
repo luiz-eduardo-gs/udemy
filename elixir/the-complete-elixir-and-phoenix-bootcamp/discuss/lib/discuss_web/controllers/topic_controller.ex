066e1fd50d790e81e005873780b57b9b0f7b773a
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-10-08 19:39:45 -0300

    feat: created topic model schema

--- elixir/the-complete-elixir-and-phoenix-bootcamp/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ elixir/the-complete-elixir-and-phoenix-bootcamp/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,6 +2,6 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   def new(conn, _params) do
-    text(conn, "Form to create a new topic")
+    render(conn, "new.html")
   end
 end
